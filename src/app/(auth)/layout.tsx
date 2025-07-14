007e5871ae49b5e587b788a169e1019a35d91685
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2025-07-14 02:42:29 -0300

    chore: improve the /training header by adding some new svg images to it

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,15 @@
 import { logout } from '@/actions/auth-actions';
 import '../globals.css';
 import SignoutButton from '@/components/auth/SignOutButton';
+import Image from 'next/image';
 
 export const metadata = {
   title: 'Training!',
   description: 'Next.js Authentication',
 };
+
+import boxingGloveLogo from '@/assets/boxing-glove.svg';
+import boxingHelmetLogo from '@/assets/boxing-helmet.svg';
 
 export default function AuthRootLayout({
   children,
@@ -15,7 +19,24 @@ export default function AuthRootLayout({
   return (
     <>
       <header id="auth-header">
-        <p>Welcome back!</p>
+        <div className="flex justify-center items-center gap-4">
+          <Image
+            src={boxingGloveLogo}
+            alt="boxing glove"
+            className="grayscale-25"
+            width={64}
+            height={64}
+          />
+          <Image
+            src={boxingHelmetLogo}
+            alt="boxing glove"
+            className="hue-rotate-135"
+            width={64}
+            height={64}
+          />
+
+          <p>Welcome back!</p>
+        </div>
         <form action={logout}>
           <SignoutButton />
         </form>
