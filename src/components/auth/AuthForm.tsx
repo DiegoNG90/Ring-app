14b404074813f4b11c6d949f2e6a72f55a29f3e9
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2025-07-14 00:15:45 -0300

    feat: brind AuthComponent and pass it into Home page

---
+++ src/components/auth/AuthForm.tsx
@@ -0,0 +1,60 @@
+'use client';
+import { auth } from '@/actions/auth-actions';
+import Link from 'next/link';
+
+import type { Errors } from '../../actions/auth-actions';
+import { useActionState } from 'react';
+
+interface AuthFormInterface {
+  mode: string;
+}
+
+const initialState: { errors: Errors } = {
+  errors: {},
+};
+
+import logo from '../../../public/assets/login.svg';
+import Image from 'next/image';
+
+export default function AuthForm({ mode }: AuthFormInterface) {
+  const [formState, action] = useActionState(
+    auth.bind(null, mode),
+    initialState
+  );
+
+  return (
+    <form id="auth-form" action={action}>
+      <div>
+        <Image src={logo} alt="A lock icon" />
+      </div>
+      <p>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+      </p>
+      <p>
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+      </p>
+      <p>
+        <button type="submit">
+          {mode === 'login' ? 'Login' : 'Create Account'}
+        </button>
+      </p>
+      {formState.errors && (
+        <ul id="form-errors">
+          {Object.keys(formState.errors).map((error: string) => (
+            <li key={error}>{formState.errors[error]}</li>
+          ))}
+        </ul>
+      )}
+      <p>
+        {mode === 'login' && (
+          <Link href="/?mode=signup">Create an account</Link>
+        )}
+        {mode === 'signup' && (
+          <Link href="/?mode=login">Login with existing account.</Link>
+        )}
+      </p>
+    </form>
+  );
+}
