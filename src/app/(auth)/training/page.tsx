7db84e12d60b48ea2b2f7eb9c3682a8ac24884b9
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2025-07-14 02:42:57 -0300

    feat: add the initial/boilerplate playBell/Ring sound to the TrainingPage

--- src/app/(auth)/training/page.tsx
+++ src/app/(auth)/training/page.tsx
@@ -1,3 +1,15 @@
-export default function TrainingPage(){
-    return <div>Training Page</div>
+'use client';
+import React from 'react';
+export default function TrainingPage() {
+  const playBell = () => {
+    const audio = new Audio('/sounds/boxing-bell.ogg');
+    audio.play().catch(console.error);
+    audio.volume = 0.5; // Esto funciona!
+  };
+  return (
+    <section>
+      <h1>Training Page</h1>
+      <button onClick={playBell}>Ring!</button>
+    </section>
+  );
 }
