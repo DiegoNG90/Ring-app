154d8742bb79b2d82fa7f33bef8f2c01160732b1
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2025-07-15 16:36:37 -0300

    chore: install shadcdn component library which is made in tailwind css

--- src/app/(auth)/training/page.tsx
+++ src/app/(auth)/training/page.tsx
@@ -1,15 +1,25 @@
 'use client';
 import React from 'react';
+import { Button } from '@/components/ui/button';
+
 export default function TrainingPage() {
   const playBell = () => {
     const audio = new Audio('/sounds/boxing-bell.ogg');
     audio.play().catch(console.error);
-    audio.volume = 0.5; // Esto funciona!
+    audio.volume = 0.5;
   };
   return (
     <section>
       <h1>Training Page</h1>
-      <button onClick={playBell}>Ring!</button>
+      <Button variant="destructive" onClick={playBell}>
+        Ring!
+      </Button>
+      <Button
+        variant="outline"
+        className="text-white bg-teal-500 hover:bg-teal-300 hover:text-white cursor-pointer"
+      >
+        Nueva rutina
+      </Button>
     </section>
   );
 }
