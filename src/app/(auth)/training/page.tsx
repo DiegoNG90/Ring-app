290d6e9fa7bfe0f9880041ec7ce19ac10626a8b2
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2025-07-17 19:48:32 -0300

    feat: create CountdownTimer client component and add it into TrainingPage

--- src/app/(auth)/training/page.tsx
+++ src/app/(auth)/training/page.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Button } from '@/components/ui/button';
 import { verifyAuth } from '@/lib/auth';
 import { redirect } from 'next/navigation';
-import BellButton from '@/components/training/BellButton';
+import SoundButton from '@/components/training/SoundButton';
+import CountdownTimer from '@/components/training/CountdownTimer/CountdownTimer';
 
 export default async function TrainingPage() {
   const result = await verifyAuth();
@@ -14,7 +15,10 @@ export default async function TrainingPage() {
   return (
     <section>
       <h1>Training Page</h1>
-      <BellButton />
+      <div className="flex justify-center items-center h-screen">
+        <CountdownTimer initialMinutes={0} initialSeconds={4} />
+      </div>
+      <SoundButton soundSrc="/sounds/boxing-bell.ogg" />
       <Button
         variant="outline"
         className="text-white bg-teal-500 hover:bg-teal-300 hover:text-white cursor-pointer"
---
+++ src/components/training/CountdownTimer/CountdownTimer.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import React, { useEffect, useState, useRef } from 'react';
+import { Button } from '@/components/ui/button'; // shadcn/ui
+
+interface CountdownTimerProps {
+  initialMinutes?: number;
+  initialSeconds: number;
+  onComplete?: () => void;
+}
+
+import { playSound } from '@/lib/sound';
+
+export default function CountdownTimer({
+  initialMinutes = 0,
+  initialSeconds,
+  onComplete,
+}: CountdownTimerProps) {
+  const initialTime = (initialMinutes * 60 + initialSeconds) * 1000;
+
+  const [timeLeft, setTimeLeft] = useState(initialTime);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    if (isRunning) {
+      intervalRef.current = setInterval(() => {
+        setTimeLeft((prev) => {
+          if (initialSeconds > 5 && prev === 5000) {
+            playSound({
+              soundSrc: '/sounds/boxing-triple-bells.ogg',
+              volume: 0.1,
+            });
+          }
+          if (prev <= 10) {
+            if (onComplete) onComplete();
+            playSound({ soundSrc: '/sounds/boxing-bell.ogg', volume: 0.3 });
+            setIsRunning(false);
+            return 0;
+          }
+          return prev - 10;
+        });
+      }, 10);
+    }
+
+    return () => {
+      if (intervalRef.current) clearInterval(intervalRef.current);
+    };
+  }, [isRunning, onComplete, initialSeconds]);
+
+  const handleToggle = () => {
+    if (timeLeft === 0 && !isRunning) {
+      setTimeLeft(initialTime);
+      setIsRunning(true);
+      return;
+    }
+    setIsRunning((prev) => !prev);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+    setTimeLeft(initialTime);
+  };
+
+  const minutes = Math.floor(timeLeft / 60000);
+  const seconds = Math.floor((timeLeft % 60000) / 1000);
+  const milliseconds = Math.floor((timeLeft % 1000) / 10);
+
+  return (
+    <div className="bg-black text-white rounded-lg p-4 text-center font-mono space-y-4">
+      <div>
+        {initialMinutes !== 0 && (
+          <span className="text-4xl">{String(minutes).padStart(2, '0')}:</span>
+        )}
+        <span className="text-4xl">{String(seconds).padStart(2, '0')}:</span>
+        <span className="text-2xl">
+          {String(milliseconds).padStart(2, '0')}
+        </span>
+      </div>
+
+      <div className="flex justify-center gap-4">
+        <Button onClick={handleToggle} variant="secondary">
+          {timeLeft === 0 && !isRunning && 'Restart'}
+          {isRunning && timeLeft > 0 && 'Pause'}
+          {!isRunning && timeLeft > 0 && 'Play'}
+        </Button>
+        <Button onClick={handleStop} variant="destructive">
+          {timeLeft === 0 ? 'Reset' : 'Stop'}
+        </Button>
+      </div>
+    </div>
+  );
+}
