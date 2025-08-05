e709ecad7d8b0524100955b107ca0fd73e6ddd5f
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2025-08-05 20:27:55 -0300

    chore: order/modularize trainings and users db repo actions inside /lib/repositories folder

--- src/lib/repositories/trainings.ts
+++ src/lib/repositories/trainings.ts
@@ -1,4 +1,4 @@
-import db from './db';
+import db from '../db';
 
 export function getAllTrainingsByUserId(userId: number) {
   const result = db
--- src/lib/repositories/user.ts
+++ src/lib/repositories/user.ts
@@ -1,4 +1,4 @@
-import db from './db';
+import db from '../db';
 
 export function createUser(email: string, password: string) {
   const result = db
