dcb48e8ac11d5babbfa6043cce7c4eaeb4de8d0d
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2025-08-05 20:38:48 -0300

    chore: deactivate SoundButton for now since seems no longer necessary

--- src/components/training/SoundButton.tsx
+++ src/components/training/SoundButton.tsx
@@ -2,13 +2,13 @@
 import React from 'react';
 
 import { Button } from '@/components/ui/button';
-import { playSound } from '@/lib/sound';
+// import { playSound } from '@/lib/sound';
 
 function SoundButton({ soundSrc }: { soundSrc: string }) {
   return (
     <Button
       variant="destructive"
-      onClick={() => playSound({ soundSrc: soundSrc, volume: 0.1 })}
+      // onClick={() => playSound({ soundSrc: soundSrc, volume: 0.1 })} -> Reemplazar por nueva clase Sound en caso que usemos este btn
     >
       Ring!
     </Button>
