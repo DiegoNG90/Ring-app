d94c269fa61e9c9ca2fcb3b3d2e588c27c165360
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2025-08-06 01:46:31 -0300

    fix: on Sound module since it can't exist on the server since the Audio object only exists on the browser

--- src/components/training/CountdownTimer/CountdownTimer.tsx
+++ src/components/training/CountdownTimer/CountdownTimer.tsx
@@ -1,18 +1,15 @@
 'use client';
 
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useMemo } from 'react';
 import { Button } from '@/components/ui/button'; // shadcn/ui
+
+import { Sound } from '@/lib/utils/sound';
 
 interface CountdownTimerProps {
   initialMinutes?: number;
   initialSeconds: number;
   onComplete?: () => void;
 }
-
-import { Sound } from '@/lib/sound';
-
-const BellSound = new Sound('/sounds/boxing-bell.ogg', 0.3);
-const TripleBellSound = new Sound('/sounds/boxing-triple-bells.ogg', 0.1);
 
 export default function CountdownTimer({
   initialMinutes = 0,
@@ -25,16 +22,25 @@ export default function CountdownTimer({
   const [isRunning, setIsRunning] = useState(false);
   const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
+  const BellSound = useMemo(
+    () => new Sound('/sounds/boxing-bell.ogg', 0.3),
+    []
+  );
+  const TripleBellSound = useMemo(
+    () => new Sound('/sounds/boxing-triple-bells.ogg', 0.1),
+    []
+  );
+
   useEffect(() => {
     if (isRunning) {
       intervalRef.current = setInterval(() => {
         setTimeLeft((prev) => {
           if (initialSeconds > 5 && prev === 5000) {
-            TripleBellSound.play();
+            TripleBellSound?.play();
           }
           if (prev <= 10) {
             if (onComplete) onComplete();
-            BellSound.play();
+            BellSound?.play();
             setIsRunning(false);
             return 0;
           }
@@ -46,7 +52,7 @@ export default function CountdownTimer({
     return () => {
       if (intervalRef.current) clearInterval(intervalRef.current);
     };
-  }, [isRunning, onComplete, initialSeconds]);
+  }, [isRunning, onComplete, initialSeconds, BellSound, TripleBellSound]);
 
   const handleToggle = () => {
     if (timeLeft === 0 && !isRunning) {
@@ -60,8 +66,8 @@ export default function CountdownTimer({
   const handleStop = () => {
     setIsRunning(false);
     setTimeLeft(initialTime);
-    BellSound.stop();
-    TripleBellSound.stop();
+    BellSound?.stop();
+    TripleBellSound?.stop();
   };
 
   const minutes = Math.floor(timeLeft / 60000);
@@ -69,7 +75,7 @@ export default function CountdownTimer({
   const milliseconds = Math.floor((timeLeft % 1000) / 10);
 
   return (
-    <div className="bg-black text-white rounded-lg p-4 text-center font-mono space-y-4 w-80">
+    <div className="bg-black text-white rounded-lg p-4 text-center font-mono space-y-4 w-4/5 md:w-3/4 lg:w-1/3">
       <div>
         {initialMinutes !== 0 && (
           <span className="text-4xl">{String(minutes).padStart(2, '0')}:</span>
